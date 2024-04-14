0cffe4fa0eb98e81c373dfbeb76389cc77d3050f
zemafaz <josemfazevedo.b@hotmail.com>
2024-04-14 17:28:38 +0100

    Add .nvim-lsp.json JSON schema at the beggining to LSP setup

--- lua/lsp-local-libraries/init.lua
+++ lua/lsp-local-libraries/init.lua
@@ -22,6 +22,11 @@ local M = {}
 
 CONFIG_FILE_NAME = ".nvim-lsp.json"
 SUPPORTED_CLIENTS = { "jsonls" }
+CONFIG_SCHEMA_PATH = require("plenary.path")
+    :new("./schemas/config-schema.json")
+    :absolute()
+
+local lspconfig = require("lspconfig")
 
 local function check_if_elem_in_list(elem, list)
     for _, list_elem in ipairs(list) do
@@ -39,37 +44,67 @@ local function get_buf_directory(bufn)
     return current_directory, current_file
 end
 
+local function append_json_schemas(schema)
+    local current_schema = lspconfig.jsonls.manager.config.settings.json.schemas
+    if current_schema then
+        table.insert(current_schema, schema)
+    else
+        current_schema = schema
+    end
+    lspconfig.jsonls.manager.config.settings.json.schemas = current_schema
+end
+
+local function add_config_file_schema()
+    local config_file_schema = {
+        fileMatch = { CONFIG_FILE_NAME },
+        url = CONFIG_SCHEMA_PATH
+    }
+    append_json_schemas(config_file_schema)
+end
+
 local function setup()
-    vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function (args)
 
-            -- Check supportted LSP
-            local client = vim.lsp.get_client_by_id(args.data.client_id)
-            if not check_if_elem_in_list(client.name, SUPPORTED_CLIENTS) then
-                return nil
-            end
+    add_config_file_schema()
 
-            -- Opening config file
-            local current_directory = get_buf_directory(args.buf)
-            local config_file_path = string.format("%s/%s", current_directory, CONFIG_FILE_NAME)
-            local config_file = io.open(config_file_path, "r")
-            if config_file == nil then
-                return nil
-            end
-
-            -- Reading config file
-            local json = require("lsp-local-libraries.json")
-            local config = json.decode(config_file:read("*a"))
-            config_file:close()
-
-            if config["jsonls"] then
-                local patterns = config["jsonls"]
-                for pattern, schemas_path_list in pairs(patterns) do
-                    print(pattern .. " : " .. vim.inspect(schemas_path_list))
-                end
-            end
+    for _, workspace_directory in ipairs(vim.lsp.buf.list_workspace_folders()) do
+        local config_file = io.open(workspace_directory .. "/" .. CONFIG_FILE_NAME)
+        if not config_file then
+            goto continue
         end
-    })
+        ::continue::
+    end
+
+
+    -- vim.api.nvim_create_autocmd("LspAttach", {
+    --     callback = function (args)
+    --
+    --         -- Check supportted LSP
+    --         local client = vim.lsp.get_client_by_id(args.data.client_id)
+    --         if not check_if_elem_in_list(client.name, SUPPORTED_CLIENTS) then
+    --             return nil
+    --         end
+    --
+    --         -- Opening config file
+    --         local current_directory = get_buf_directory(args.buf)
+    --         local config_file_path = string.format("%s/%s", current_directory, CONFIG_FILE_NAME)
+    --         local config_file = io.open(config_file_path, "r")
+    --         if config_file == nil then
+    --             return nil
+    --         end
+    --
+    --         -- Reading config file
+    --         local json = require("lsp-local-libraries.json")
+    --         local config = json.decode(config_file:read("*a"))
+    --         config_file:close()
+    --
+    --         if config["jsonls"] then
+    --             local patterns = config["jsonls"]
+    --             for pattern, schemas_path_list in pairs(patterns) do
+    --                 print(pattern .. " : " .. vim.inspect(schemas_path_list))
+    --             end
+    --         end
+    --     end
+    -- })
 end
 
 M.setup = setup
